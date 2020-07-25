130543ee46d5f7e81e6fe67678f9aa47d6b87459
Ashwin Prasad <66101222+Perspekt@users.noreply.github.com>
2020-07-25 09:56:36 -0400

    Add comments and clean

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
+# Update and download packages
 sudo apt-get update -y && apt-get install -y libleveldb-dev build-essential
 sudo apt-get install golang-go -y
 sudo apt-get install nginx -y
 sudo apt-get install git -y
 
-
+# Download source
 git clone https://github.com/pokt-network/pocket-core.git
 cd pocket-core
 git checkout tags/RC-0.4.3
 
-which go
-GOPATH=/usr/bin/go
-
+# Compile Source (Var not necessary)
+GOPATH=$(which go)
 go build -tags cleveldb -o /usr/bin/pocket ./app/cmd/pocket_core/main.go
 
+# Create .pocket dir and download genesis.json file
 mkdir -p .pocket/config && cd .pocket/config; curl -O https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/testnet/genesis.json
 
-curl -O https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/testnet/genesis.json > ~/.pocket/config/genesis.json
-
-#wget https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/testnet/genesis.json -P ~/.pocket/config
 
 cp ./config.json ~/.pocket/config/
 cp ./chains.json ~/.pocket/config/
